b5597e7ef8f9885e0acd7cd4bcd4395cd4d16d82
kkamal11 <kishorkamal7091@gmail.com>
2026-04-04 18:22:22 +0530

    infix-postfix

---
+++ StackQueue/expression.py
@@ -0,0 +1,50 @@
+class ExpressionConverter:
+
+    def precedence(self, operator: str):
+
+        if operator == "^":
+            return 3
+        if operator in ["*", "/"]:
+            return 2
+        if operator in ["+", "-"]:
+            return 1
+        return -1
+
+    def infix_to_postfix(self, exp: str):
+        ans = []
+        stack = []
+
+        for ch in exp:
+            if ch == " ":
+                continue
+            if ch.isalnum():
+                ans.append(ch)
+            else:
+                if ch == ")":
+                    while stack and stack[-1] != "(":
+                        ans.append(stack.pop())
+                    stack.pop()
+                elif (
+                    not stack
+                    or self.precedence(ch) > self.precedence(stack[-1])
+                    or ch == "("
+                ):
+                    stack.append(ch)
+                else:
+                    while stack and self.precedence(stack[-1]) >= self.precedence(ch):
+                        ans.append(stack.pop())
+                    stack.append(ch)
+
+        while stack:
+            ans.append(stack.pop())
+
+        return "".join(ans)
+
+
+conv = ExpressionConverter()
+exp1 = " a + b * (c^d - e) ^ (f + g * h) - i "
+exp2 = " (p + q) * (m - n)"
+exp3 = "a + b * (c ^ d - e)"
+print(conv.infix_to_postfix(exp1))
+print(conv.infix_to_postfix(exp2))
+print(conv.infix_to_postfix(exp3))
