a426a79f242b4bba9ecaca5b0b787a0557bfb890
kkamal11 <kishorkamal7091@gmail.com>
2026-04-05 07:32:02 +0530

    sum-subarr-mins

---
+++ StackQueue/sumSubarrayMins.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    MOD = 10**9 + 7
+
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        sum_ = 0
+        n = len(arr)
+
+        for i in range(n):
+            min_ = float("inf")
+            for j in range(i, n):
+                min_ = min(min_, arr[j])
+                sum_ += min_ % self.MOD
+
+        return sum_
+
+    def sumSubarrayMinsOptimized(self, arr: List[int]) -> int:
+        pass
+
+
+sol = Solution()
+arr = [11, 81, 94, 43, 3]
+print(sol.sumSubarrayMins(arr))
