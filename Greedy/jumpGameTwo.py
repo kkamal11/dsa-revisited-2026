338ffdfde805743fafc3ba3967f80b94897d0358
kkamal11 <kishorkamal7091@gmail.com>
2026-04-07 08:17:51 +0530

    greedy-jump-game

--- Greedy/jumpGameTwo.py
+++ Greedy/jumpGameTwo.py
@@ -2,27 +2,52 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        max_far = 0
+    
         n = len(nums)
-        step = 0
+        jumps = 0
+        left = 0
+        right = 0
 
-        for i in range(n):
-            curr_far = i + nums[i]
-            max_far = max(max_far, curr_far)
-            if i > max_far:
-                return -1
+        while right < n-1:
+            farthest = 0
+            for j in range(left, right+1):
+                farthest = max(farthest, j+ nums[j])
+            left = right + 1
+            right = farthest
+            jumps += 1
 
-            if i == max_far:
-                step += 1
-                
-        return step
-            
+        return jumps
+        
+    def jump2(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return 0
+
+        jumps = 0
+        curr_end = 0   # end of current jump range
+        max_reach = 0  # farthest index we can reach while scanning this range
+
+        # We only need to process until n-2, because once we can reach last index,
+        # we stop. No need to jump from the last index itself.
+        for i in range(n - 1):
+            max_reach = max(max_reach, i + nums[i])
+
+            # Time to take one jump: we finished scanning current range.
+            if i == curr_end:
+                jumps += 1
+                curr_end = max_reach
+
+                # Optimization: if this jump can reach/past last index, done.
+                if curr_end >= n - 1:
+                    break
+
+        return jumps
 
 
 
 sol = Solution()
-nums = [2,3,1,1,4]
+nums = [2,3,4,1,0,1]
 print(sol.jump(nums))
 
-nums = [2,3,0,1,4]
-print(sol.jump(nums))
+# nums = [2,3,0,1,4]
+# print(sol.jump(nums))
