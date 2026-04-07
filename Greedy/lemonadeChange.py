72d4241e6331a869f5607055d3170f7fb62e07b3
kkamal11 <kishorkamal7091@gmail.com>
2026-04-07 05:56:15 +0530

    greedy-coins

--- Greedy/lemonadeChange.py
+++ Greedy/lemonadeChange.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        in_hand = {5:0, 10:10, 20:0}
+        in_hand = {5:0, 10:0, 20:0}
         
         for paym in bills:
             if paym == 5:
@@ -16,16 +16,17 @@ class Solution:
             else:
                 if in_hand[10] > 0 and in_hand[5] > 0:
                     in_hand[20] += 1
-                    in_hand[10] -=1
+                    in_hand[10] -= 1
                     in_hand[5] -= 1
                 elif in_hand[5] >= 3:
                     in_hand[5] -= 3
                 else:
                     return False
+            print(in_hand)
         return True
 
 sol = Solution()
 bills = [5,5,10,10,20]
-print(sol.lemonadeChange(bills))
+# print(sol.lemonadeChange(bills))
 bills = [5,5,5,10,5,5,10,20,20,20]
 print(sol.lemonadeChange(bills))
