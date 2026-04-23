e18ae09520023f7d7bc6661d8896e3b4622de992
kkamal11 <kishorkamal7091@gmail.com>
2026-04-23 06:15:46 +0530

    num-island

---
+++ Graph/isBipartite.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [-1] * n
+
+        def dfs(node, c):
+            color[node] = c
+
+            for nei in graph[node]:
+                if color[nei] == -1:
+                    if not dfs(nei, 1 - c):
+                        return False
+                elif color[nei] == c:
+                    return False
+
+            return True
+
+        for i in range(n):
+            if color[i] == -1:
+                if not dfs(i, 0):
+                    return False
+
+        return True
+
+
+sol = Solution()
+graph = [[1, 3], [0, 2], [1, 3], [0, 2]]
+print(sol.isBipartite(graph))
