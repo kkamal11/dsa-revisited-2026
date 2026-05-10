ea2fe9efbdd1726b3266058c9eba14ab51ec3ae2
kkamal11 <kishorkamal7091@gmail.com>
2026-05-10 10:49:42 +0530

    up

--- Greedy/coinChange.py
+++ Greedy/coinChange.py
@@ -32,6 +32,17 @@ class Solution:
 
         return total_coins if amount == 0 else -1
 
+    def coinChangeDP(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+
+        # Fill the dp table
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+
+        return dp[amount] if dp[amount] != float("inf") else -1
+
 
 sol = Solution()
 coins = [1, 3, 4]
