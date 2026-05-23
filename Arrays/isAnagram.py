f846a09e25325bbbf9b8d60ac1fab32798d43907
kkamal11 <kishorkamal7091@gmail.com>
2026-05-23 08:29:19 +0530

    group-anagram-III

--- Arrays/isAnagram.py
+++ Arrays/isAnagram.py
@@ -19,3 +19,21 @@ def isAnagram(s: str, t: str) -> bool:
 
 def isAnagram(s: str, t: str) -> bool:
     return sorted(s) == sorted(t)
+
+
+def isAnagram(self, s: str, t: str) -> bool:
+    mapp = {}
+    for ch in s:
+        mapp[ch] = mapp.get(ch, 0) + 1
+
+    for ch in t:
+        if ch not in mapp or mapp[ch] == 0:
+            return False
+
+        mapp[ch] -= 1
+
+    for k in mapp:
+        if mapp[k] != 0:
+            return False
+
+    return True
