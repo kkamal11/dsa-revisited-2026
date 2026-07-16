2a927ee0928610c6e3420ec537a5df619b8cf768
kkamal11 <kishorkamal7091@gmail.com>
2026-07-16 07:52:50 +0530

    gcd-sum

---
+++ LeetDaily/gcdSum.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def gcd(self, a: int, b: int) -> int:
+        while b != 0:
+            a, b = b, a % b
+        return a
+
+    def gcdSum(self, nums: list[int]) -> int:
+        prefix_gcd = []
+        mxi = float("-inf")
+
+        for num in nums:
+            mxi = max(mxi, num)
+            prefix_gcd.append(self.gcd(num, mxi))
+
+        prefix_gcd.sort()
+
+        n = len(prefix_gcd)
+        result = 0
+
+        for start in range(n // 2):
+            end = n - start - 1
+            if start != end:
+                result += self.gcd(prefix_gcd[start], prefix_gcd[end])
+
+        return result
