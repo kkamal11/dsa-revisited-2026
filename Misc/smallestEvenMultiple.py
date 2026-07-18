84e102112bf5853e736fc0f9cb9b7b07a49cc729
kkamal11 <kishorkamal7091@gmail.com>
2026-07-18 08:01:04 +0530

    smallest-even-multiple

---
+++ Misc/smallestEvenMultiple.py
@@ -0,0 +1,11 @@
+"""
+Given a positive integer n, return the smallest positive integer
+that is a multiple of both 2 and n.
+"""
+
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        return 2 * n
