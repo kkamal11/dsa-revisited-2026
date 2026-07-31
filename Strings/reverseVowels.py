392d9351224684091df4b2d2ae4439252902bb84
kkamal11 <kishorkamal7091@gmail.com>
2026-08-01 03:28:47 +0530

    arrange-vowels

--- Strings/reverseVowels.py
+++ Strings/reverseVowels.py
@@ -14,3 +14,22 @@ class Solution:
                 ans.append(ch)
 
         return "".join(ans)
+
+    def reverseVowels(self, s: str) -> str:
+        vowels = {"a", "e", "i", "o", "u"}
+
+        chars = list(s)
+        left, right = 0, len(chars) - 1
+
+        while left < right:
+            while left < right and chars[left].lower() not in vowels:
+                left += 1
+
+            while left < right and chars[right].lower() not in vowels:
+                right -= 1
+
+            chars[left], chars[right] = chars[right], chars[left]
+            left += 1
+            right -= 1
+
+        return "".join(chars)
