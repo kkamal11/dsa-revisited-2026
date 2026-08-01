6939d6da4e8d4e347d26cddd2053d5400b603941
kkamal11 <kishorkamal7091@gmail.com>
2026-08-01 07:48:22 +0530

    intersection-array

---
+++ Arrays/intersection.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+
+        m = len(nums1)
+        n = len(nums2)
+
+        i, j = 0, 0
+
+        ans = set()
+
+        while i < m and j < n:
+            if nums1[i] == nums2[j]:
+                ans.add(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+
+        return list(ans)
