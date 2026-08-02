6dd570e615da67b737c1c6e0793debf347694ec5
kkamal11 <kishorkamal7091@gmail.com>
2026-08-02 06:36:41 +0530

    binary-tree-paths

--- Trees/binaryTreePaths.py
+++ Trees/binaryTreePaths.py
@@ -30,3 +30,23 @@ class Solution:
                 q.append([node.right, path + "->" + str(node.right.val)])
 
         return ans
+
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        ans = []
+
+        def dfs(node, path):
+            if not node:
+                return
+
+            path += str(node.val)
+
+            if not node.left and not node.right:
+                ans.append(path)
+                return
+
+            path += "->"
+            dfs(node.left, path)
+            dfs(node.right, path)
+
+        dfs(root, "")
+        return ans
