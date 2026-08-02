e10759262ede08200a258f803ac82b90f4fded46
kkamal11 <kishorkamal7091@gmail.com>
2026-08-02 07:18:32 +0530

    arrange-coins

--- BS/arrangeCoins.py
+++ BS/arrangeCoins.py
@@ -13,17 +13,20 @@ class Solution:
         lo = 0
         hi = n
 
-        while lo < hi:
+        while lo <= hi:
             mid = (lo + hi) // 2
-            coins = mid * (mid + 1) / 2
-            if coins <= n:
-                lo = mid
-            if coins > n:
+            coins = mid * (mid + 1) // 2
+
+            if coins == n:
+                return mid
+            elif coins < n:
+                lo = mid + 1
+            else:
                 hi = mid - 1
 
-        return lo
+        return hi
 
 
 sol = Solution()
-n = 6
+n = 8
 print(sol.arrangeCoins1(n), sol.arrangeCoins(n))
