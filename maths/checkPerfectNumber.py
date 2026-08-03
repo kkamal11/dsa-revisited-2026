d79e47b790ee5e4c8619d9d7edaadba487f94ad7
kkamal11 <kishorkamal7091@gmail.com>
2026-08-03 19:45:58 +0530

    perfect-number

--- maths/checkPerfectNumber.py
+++ maths/checkPerfectNumber.py
@@ -9,9 +9,26 @@ class Solution:
         return num == sum_
 
     """
-    
+    Optimal Approach:
+    Divisors occur in pairs: if i divides num, then num // i is also a divisor.
+    Iterate only up to √num and add both divisors whenever a factor is found.
+    If both divisors are the same (i == num // i), add it only once.
+    If the sum of all proper divisors equals num, return True; otherwise, return False.
     
     """
 
     def checkPerfectNumber(self, num: int) -> bool:
-        pass
+        if num <= 1:
+            return False
+
+        total = 1
+        i = 2
+
+        while i * i <= num:
+            if num % i == 0:
+                total += i
+                if i != num // i:
+                    total += num // i
+            i += 1
+
+        return num == total
