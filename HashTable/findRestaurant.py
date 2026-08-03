6186f6d7d48d79a33839041e90d3b23e40677fa7
kkamal11 <kishorkamal7091@gmail.com>
2026-08-03 20:14:34 +0530

    find-restaurant

--- HashTable/findRestaurant.py
+++ HashTable/findRestaurant.py
@@ -19,3 +19,43 @@ class Solution:
                     ans.append(list1[i])
 
         return ans
+
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+
+        l1, l2 = len(list1), len(list2)
+        min_idx_sum = float("inf")
+        ans = []
+        mapp = {}
+
+        for i in range(l1):
+            mapp[list1[i]] = i
+
+        for j in range(l2):
+            if list2[j] in mapp:
+                if mapp[list2[j]] + j < min_idx_sum:
+                    min_idx_sum = mapp[list2[j]] + j
+                    while ans:
+                        ans.pop()
+                    ans.append(list2[j])
+                elif min_idx_sum == mapp[list2[j]] + j:
+                    ans.append(list2[j])
+
+        return ans
+
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+
+        min_idx_sum = float("inf")
+        ans = []
+        mapp = {s: i for i, s in enumerate(list1)}
+
+        for j in range(len(list2)):
+            s = list2[j]
+            if s in mapp:
+                idx = mapp[s]
+                if idx + j < min_idx_sum:
+                    min_idx_sum = idx + j
+                    ans = [s]
+                elif min_idx_sum == idx + j:
+                    ans.append(s)
+
+        return ans
