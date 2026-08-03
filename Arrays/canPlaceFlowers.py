32324540397d76617411487c0200b6a7fad9a44b
kkamal11 <kishorkamal7091@gmail.com>
2026-08-03 17:54:25 +0530

    can-place-flower

--- Arrays/canPlaceFlowers.py
+++ Arrays/canPlaceFlowers.py
@@ -31,3 +31,20 @@ class Solution:
                 return True
 
         return n == 0
+
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        m = len(flowerbed)
+
+        for i in range(m):
+            if flowerbed[i] == 0:
+                left = i == 0 or flowerbed[i - 1] == 0
+                right = i == m - 1 or flowerbed[i + 1] == 0
+
+                if left and right:
+                    flowerbed[i] = 1
+                    n -= 1
+
+                    if n == 0:
+                        return True
+
+        return n <= 0
