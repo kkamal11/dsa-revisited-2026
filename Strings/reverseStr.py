39f0f7b13adcbf9a57035d1acc6bf8b19e665190
kkamal11 <kishorkamal7091@gmail.com>
2026-08-07 07:10:05 +0530

    reverse-str

--- Strings/reverseStr.py
+++ Strings/reverseStr.py
@@ -28,3 +28,29 @@ class Solution:
             ans.append(tmp)
 
         return "".join(ans)
+
+    def reverseStr(self, s: str, k: int) -> str:
+
+        ans = []
+
+        count = 0
+        q = deque([])
+        rev = True
+
+        for ch in s:
+            q.append(ch)
+            count += 1
+
+            if count == k:
+                while q:
+                    if rev:
+                        ans.append(q.pop())
+                    else:
+                        ans.append(q.popleft())
+                rev = not rev
+                count = 0
+
+        while q:
+            ans.append(q.pop()) if rev else ans.append(q.popleft())
+
+        return "".join(ans)
