b2433b39b63c023c0a85d5a0ba31ce359dd23295
kkamal11 <kishorkamal7091@gmail.com>
2026-08-11 05:11:51 +0530

    backspace-comparison

--- StackQueue/backspaceCompare.py
+++ StackQueue/backspaceCompare.py
@@ -4,15 +4,20 @@ class Solution:
         t_stack = []
 
         for ch in s:
-            if ch == "#" and s_stack:
-                s_stack.pop()
-            elif ch != "#":
+            if ch == "#":
+                if s_stack:
+                    s_stack.pop()
+            else:
                 s_stack.append(ch)
 
         for ch in t:
-            if ch == "#" and t_stack:
-                t_stack.pop()
-            elif ch != "#":
+            if ch == "#":
+                if t_stack:
+                    t_stack.pop()
+            else:
                 t_stack.append(ch)
 
-        return len(s_stack) == len(t_stack) and s_stack == t_stack
+        # return len(s_stack) == len(t_stack) and s_stack == t_stack
+        return (
+            s_stack == t_stack
+        )  # This internally compares the length first and then each element
