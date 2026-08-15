314c862cfaaebd1483e443f947c4971e26d84219
kkamal11 <kishorkamal7091@gmail.com>
2026-08-15 08:57:47 +0530

    pivot-idx-array

--- Arrays/pivotIndex.py
+++ Arrays/pivotIndex.py
@@ -1,4 +1,6 @@
 from typing import List
+
+
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
 
@@ -8,17 +10,34 @@ class Solution:
 
         for i in range(1, n):
             left_sum[i] = left_sum[i - 1] + nums[i - 1]
-    
+
         for i in range(n - 1, -1, -1):
             if i != n - 1:
                 right_sum[i] = right_sum[i + 1] + nums[i + 1]
-        
+
         for i in range(n):
             ls = left_sum[i]
             rs = right_sum[i]
             if ls == rs:
                 return i
-        
+
         return -1
-            
-        
+
+    def pivotIndex(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        left_sum = [0] * n
+        right_sum = [0] * n
+
+        for i in range(n - 1, -1, -1):
+            if i != n - 1:
+                right_sum[i] = right_sum[i + 1] + nums[i + 1]
+
+        for i in range(n):
+            if i != 0:
+                left_sum[i] = left_sum[i - 1] + nums[i - 1]
+
+            if left_sum[i] == right_sum[i]:
+                return i
+
+        return -1
