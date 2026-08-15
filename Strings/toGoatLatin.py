fc84898cd660b0acbe438bcbd38da422bec71d8e
kkamal11 <kishorkamal7091@gmail.com>
2026-08-15 09:13:17 +0530

    pivot-idx-array

--- Strings/toGoatLatin.py
+++ Strings/toGoatLatin.py
@@ -2,13 +2,13 @@ class Solution:
     def toGoatLatin(self, sentence: str) -> str:
         a = 0
         ans = []
-        vowels = {"A", "E", "I", "O", "U"}
+        vowels = set("aeiouAEIOU")
 
         words = sentence.split()
 
         for word in words:
             a += 1
-            if word[0].upper() in vowels:
+            if word[0] in vowels:
                 transformed_word = word + "ma" + "a" * a
             else:
                 transformed_word = word[1:] + word[0] + "ma" + "a" * a
