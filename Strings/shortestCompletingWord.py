d040c3e285e202925c5fd79b289a5155d465f4bf
kkamal11 <kishorkamal7091@gmail.com>
2026-08-15 17:42:05 +0530

    shortest-completing-word

--- Strings/shortestCompletingWord.py
+++ Strings/shortestCompletingWord.py
@@ -20,3 +20,19 @@ class Solution:
                     ans = word
 
         return ans
+
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        d = Counter(ch.lower() for ch in licensePlate if ch.isalpha())
+
+        ans = ""
+
+        for word in words:
+            wd = Counter(word)
+            for w in d:
+                if w.isalpha() and (w not in wd or wd[w] < d[w]):
+                    break
+            else:
+                if not ans or len(word) < len(ans):
+                    ans = word
+
+        return ans
