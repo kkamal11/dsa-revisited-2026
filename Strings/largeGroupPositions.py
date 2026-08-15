3bf9538a9d173e96c775d9c2f4cec5735c25619e
kkamal11 <kishorkamal7091@gmail.com>
2026-08-15 10:37:12 +0530

    large-grp-position

--- Strings/largeGroupPositions.py
+++ Strings/largeGroupPositions.py
@@ -3,6 +3,10 @@ from typing import List
 
 class Solution:
     def largeGroupPositions(self, s: str) -> List[List[int]]:
+
+        if s == "":
+            return []
+
         ans = []
 
         last = None
