166000d20943ace72df6e6298863516dad02b4c4
kkamal11 <kishorkamal7091@gmail.com>
2026-08-19 09:58:19 +0530

    number-arihmetic-slices

--- Arrays/numberOfArithmeticSlices.py
+++ Arrays/numberOfArithmeticSlices.py
@@ -24,7 +24,17 @@ class Solution:
         return count
 
     def numberOfArithmeticSlices2(self, nums: List[int]) -> int:
-        pass
+        count = 0
+        curr = 0
+
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
+                curr += 1
+                count += curr
+            else:
+                curr = 0
+
+        return count
 
 
 sol = Solution()
