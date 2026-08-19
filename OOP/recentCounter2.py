ae5a158eca9e54e5401823e26832320cb1b2deb1
kkamal11 <kishorkamal7091@gmail.com>
2026-08-19 18:04:45 +0530

    recent-counter

---
+++ OOP/recentCounter2.py
@@ -0,0 +1,20 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.counter = []
+
+    def ping(self, t: int) -> int:
+        self.counter.append(t)
+        count = 0
+        n = len(self.counter) - 1
+
+        while n >= 0 and self.counter[n] >= (t - 3000):
+            count += 1
+            n -= 1
+
+        return count
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
