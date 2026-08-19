7c71f6bdb1a84b56d9794f525599652d3a11a77a
kkamal11 <kishorkamal7091@gmail.com>
2026-08-19 17:50:47 +0530

    sortArrayByParity

---
+++ Arrays/sortArrayByParityII.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums)
+
+        even_idx = 0
+        odd_idx = 1
+
+        for num in nums:
+            if num % 2 == 0:
+                ans[even_idx] = num
+                even_idx += 2
+            else:
+                ans[odd_idx] = num
+                odd_idx += 2
+
+        return ans
