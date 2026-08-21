ab1e5538cb43ab39a68fa96d2665de3108a1564b
kkamal11 <kishorkamal7091@gmail.com>
2026-08-21 17:49:17 +0530

    max-product-string

--- Arrays/maxProductStringLength.py
+++ Arrays/maxProductStringLength.py
@@ -43,3 +43,25 @@ class Solution:
                     ans = max(ans, len(words[i]) * len(words[j]))
 
         return ans
+
+    def maxProduct(self, words: List[str]) -> int:
+        n = len(words)
+
+        masks = []
+
+        for word in words:
+            mask = 0
+
+            for ch in word:
+                mask |= 1 << (ord(ch) - ord("a"))
+
+            masks.append(mask)
+
+        ans = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if masks[i] & masks[j] == 0:
+                    ans = max(ans, len(words[i]) * len(words[j]))
+
+        return ans
