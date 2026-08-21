9fc20db8aa58b16b1a37bd85c8aa4949d7bbcb11
kkamal11 <kishorkamal7091@gmail.com>
2026-08-21 17:36:03 +0530

    max-product-string

---
+++ Arrays/maxProductStringLength.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        ans = 0
+        n = len(words)
+
+        for i in range(n):
+            w1 = words[i]
+            for j in range(i + 1, n):
+                w2 = set(words[j])
+                for k in w1:
+                    if k in w2:
+                        break
+                else:
+                    ans = max(ans, len(w1) * len(words[j]))
+        return ans
