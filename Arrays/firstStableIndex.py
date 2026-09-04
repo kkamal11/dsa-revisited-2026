f37d27152e3b35f212bea20ee644111a422f0c19
kkamal11 <kishorkamal7091@gmail.com>
2026-09-04 23:17:22 +0530

    first-stable-idx

--- Arrays/firstStableIndex.py
+++ Arrays/firstStableIndex.py
@@ -10,3 +10,21 @@ class Solution:
                 return i
 
         return -1
+
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        suffix_min = [0] * n
+        suffix_min[-1] = nums[-1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(suffix_min[i + 1], nums[i])
+
+        maxi = nums[0]
+
+        for i in range(n):
+            maxi = max(maxi, nums[i])
+            if maxi - suffix_min[i] <= k:
+                return i
+
+        return -1
