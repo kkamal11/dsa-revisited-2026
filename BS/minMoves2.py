c80b4faaf982fee9eb699c7e90bf8f3b233b5a76
kkamal11 <kishorkamal7091@gmail.com>
2026-09-08 21:37:13 +0530

    min-moves-2

--- BS/minMoves2.py
+++ BS/minMoves2.py
@@ -15,3 +15,19 @@ class Solution:
             moves = min(moves, curr)
 
         return moves
+
+    def minMoves2(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        moves = float("inf")
+
+        for i in range(n):
+            curr = 0
+            for j in range(n):
+                if i != j:
+                    curr += abs(nums[i] - nums[j])
+                    if curr > moves:
+                        break
+            moves = min(moves, curr)
+
+        return moves
