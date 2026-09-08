3c7b36b3d75b31d30626ad347768f319691bf44f
kkamal11 <kishorkamal7091@gmail.com>
2026-09-08 21:47:40 +0530

    min-moves-2

--- BS/minMoves2.py
+++ BS/minMoves2.py
@@ -31,3 +31,22 @@ class Solution:
             moves = min(moves, curr)
 
         return moves
+
+    """
+    Observation: The optimal solution is to find the median of the array and calculate the sum of absolute differences from the median.
+    This is because the median minimizes the sum of absolute deviations.
+    """
+
+    def minMoves2(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        moves = 0
+        nums.sort()
+
+        median = nums[n // 2]
+
+        for i in range(n):
+            if i != n // 2:
+                moves += abs(median - nums[i])
+
+        return moves
