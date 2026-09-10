74603d79fa9966713ec5728c14b49ed9c19acd38
kkamal11 <kishorkamal7091@gmail.com>
2026-09-10 23:12:56 +0530

    avg-subtree-equal

--- Trees/averageOfSubtree.py
+++ Trees/averageOfSubtree.py
@@ -41,3 +41,25 @@ class Solution:
                 q.append(node.right)
 
         return count
+
+    def averageOfSubtreeDFS(self, root: TreeNode) -> int:
+        count = 0
+
+        def dfs(node):
+            nonlocal count
+            if not node:
+                return (0, 0)  # sum, count
+
+            left_sum, left_count = dfs(node.left)
+            right_sum, right_count = dfs(node.right)
+
+            total_sum = left_sum + right_sum + node.val
+            total_count = left_count + right_count + 1
+
+            if node.val == total_sum // total_count:
+                count += 1
+
+            return (total_sum, total_count)
+
+        dfs(root)
+        return count
