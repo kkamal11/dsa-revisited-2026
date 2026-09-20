d9ad0c37308cf784fef523ee13dc165c85a4d593
kkamal11 <kishorkamal7091@gmail.com>
2026-09-20 09:28:43 +0530

    reverse-degree-string

--- Strings/reverseDegree.py
+++ Strings/reverseDegree.py
@@ -7,3 +7,12 @@ class Solution:
             reverse_degree += (ord("z") - ord(ch) + 1) * (idx + 1)
 
         return reverse_degree
+
+    def reverseDegree(self, s: str) -> int:
+        reverse_degree = 0
+        ord_z = 123
+        _ord = ord
+        for idx, ch in enumerate(s, 1):
+            reverse_degree += (ord_z - _ord(ch)) * idx
+
+        return reverse_degree
