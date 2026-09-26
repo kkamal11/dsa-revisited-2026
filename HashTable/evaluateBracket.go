4af234b4c08ce3aafb2efe7ec2bbeaef935c2d4d
kkamal11 <kishorkamal7091@gmail.com>
2026-09-26 11:24:33 +0530

    evaluate-bracket

--- HashTable/evaluateBracket.go
+++ HashTable/evaluateBracket.go
@@ -37,3 +37,41 @@ func evaluate(s string, knowledge [][]string) string {
 
 	return result
 }
+
+func evaluate(s string, knowledge [][]string) string {
+
+	var result []string
+	var string_key []string
+	var str string
+	var opened bool
+	knowledge_map := make(map[string]string, len(knowledge))
+
+	for _, k := range knowledge {
+		key, val := k[0], k[1]
+		knowledge_map[key] = val
+	}
+
+	for _, st := range s {
+		str = string(st)
+
+		if str == "(" {
+			opened = true
+
+		} else if str == ")" {
+			val, exists := knowledge_map[strings.Join(string_key, "")]
+			if !exists {
+				val = "?"
+			}
+			result = append(result, val)
+			opened, string_key = false, nil
+
+		} else if opened {
+			string_key = append(string_key, str)
+
+		} else {
+			result = append(result, str)
+		}
+	}
+
+	return strings.Join(result, "")
+}
